ad607ffe9a2c41ec91d055462921d336485a996f
jffvicente <vissente@gmail.com>
2023-03-01 21:17:58 +0100

    funcao jogada

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-console.log("pipocas")
-//funcao chamada qd se clica na celula
-function teste(context){
-    console.log(context)
+class Jogador {
+    constructor (nome,simbolo) {
+        this.nome = nome
+        this.simbolo = simbolo
+    }
+    compara(jogador) {
+        return this.nome === jogador.nome && this.simbolo === jogador.simbolo
+    }
 }
+const jogador1 = new Jogador("A","X")
+const jogador2 = new Jogador("B","O")
+let jogadoractual = jogador1
+
+function jogada(div){
+    if (div.innerText === '' ){
+
+    div.innerText = jogadoractual.simbolo
+    if (jogadoractual.compara(jogador1)) {
+        jogadoractual = jogador2
+    }
+    else{
+        jogadoractual = jogador1
+    }
+}
+}
+
+// tpc verificar vitoria empate ou derrota
